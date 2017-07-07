f53ac36af61f6680baa0efdc035aeddb13191171
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-07-07 23:20:53 +0900

    Add restore_cookies methods to Capybara::Session

--- lib/capybara/sessionkeeper.rb
+++ lib/capybara/sessionkeeper.rb
@@ -3,11 +3,24 @@ require "capybara/sessionkeeper/version"
 
 module Capybara
   module Sessionkeeper
+    class CookieError < StandardError; end
+
     def save_cookies(path = nil)
       path = prepare_path(path, 'cookies.txt')
-      data = driver.browser.manage.all_cookies
-      File.write(path, data, mode: 'wb')
+      data = Marshal.dump driver.browser.manage.all_cookies
+      File.write(path, data)
       path
+    end
+
+    def restore_cookies(path)
+      raise CookieError, "visit must be performed to restore cookies" if driver.browser.manage.all_cookies.empty?
+      data = File.read(path)
+      Marshal.load(data).each do |d|
+        driver.browser.manage.add_cookie d
+      end
+      driver.browser.manage.all_cookies
+    rescue => e
+      raise $!, "You need to visit the site you are trying to restore cookie first\n#{$!}", $!.backtrace
     end
   end
 end
--- spec/capybara/sessionkeeper_spec.rb
+++ spec/capybara/sessionkeeper_spec.rb
@@ -13,4 +13,33 @@ RSpec.describe Capybara::Sessionkeeper do
       expect(path).to match(/capybara-\d+.cookies.txt/)
     end
   end
+
+  describe '#restore_cookies' do
+    let(:cookie_path) { 'spec/fixtures/github.cookies.txt' }
+
+    it "restores cookies from file" do
+      session = Capybara::Session.new(:chrome)
+      expect(session.driver.browser.manage.all_cookies).to be_empty
+      session.visit 'https://github.com/'
+      cookies = session.restore_cookies(cookie_path)
+      expect(cookies).not_to be_empty
+      expect(cookies.all?{|c| c[:domain] =~ /github\.com/ }).to be_truthy
+    end
+
+    it "raises error when the target site has never been visited" do
+      session = Capybara::Session.new(:chrome)
+      expect(session.driver.browser.manage.all_cookies).to be_empty
+      session.visit 'https://www.google.com/'
+      expect{
+        session.restore_cookies(cookie_path)
+      }.to raise_error(Selenium::WebDriver::Error::InvalidCookieDomainError, /visit/)
+    end
+
+    it "raises error when visit has never been performed" do
+      session = Capybara::Session.new(:chrome)
+      expect{
+        session.restore_cookies(cookie_path)
+      }.to raise_error(Capybara::Sessionkeeper::CookieError, /visit/)
+    end
+  end
 end
