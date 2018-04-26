438fd570ae9a3e1db1919f674a006cf04ea15e2f
Kazuho Yamaguchi <kzh.yap@gmail.com>
2018-04-26 16:32:29 +0900

    Support poltergeist driver
    Tested with
    
    poltergeist (1.17.0)
    capybara (2.14.4)
    websocket-driver (0.7.0)
    
    $ phantomjs --version
    2.1.1

--- lib/capybara/sessionkeeper.rb
+++ lib/capybara/sessionkeeper.rb
@@ -7,7 +7,7 @@ module Capybara
 
     def save_cookies(path = nil)
       path = prepare_path(path, cookie_file_extension)
-      data = Marshal.dump driver.browser.manage.all_cookies
+      data = Marshal.dump cookies_from_driver(driver)
       File.open(path, 'wb') {|f| f.puts(data) }
       path
     end
@@ -17,14 +17,14 @@ module Capybara
       path ||= find_latest_cookie_file
       return nil if path.nil?
       data = File.open(path, 'rb') {|f| f.read }
-      Marshal.load(data).each do |d|
+      Marshal.load(data).each do |cookie|
         begin
-          driver.browser.manage.add_cookie d
+          set_cookie_for_driver(driver, cookie)
         rescue => e
           skip_invalid_cookie_domain_error(e)
         end
       end
-      driver.browser.manage.all_cookies
+      cookies_from_driver(driver)
     end
 
     def cookie_file_extension
@@ -45,6 +45,24 @@ module Capybara
         # puts "Skipped invalid cookie domain: #{d[:domain]} - #{d.inspect}"
       else
         raise(e)
+      end
+    end
+
+    def cookies_from_driver(driver)
+      if driver.class == Capybara::Poltergeist::Driver
+        driver.cookies.map do |cookie|
+          Hash[%w[name value path domain expires secure?].map{|k| [k.chomp('?').to_sym, cookie.last.send(k)] }]
+        end
+      else
+        driver.browser.manage.all_cookies
+      end
+    end
+
+    def set_cookie_for_driver(driver, cookie)
+      if driver.class == Capybara::Poltergeist::Driver
+        driver.browser.set_cookie(cookie)
+      else
+        driver.browser.manage.add_cookie(cookie)
       end
     end
   end
