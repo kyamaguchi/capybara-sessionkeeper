07e472c12e022ce07e33b0b2f8428b63e180257d
Kazuho Yamaguchi <kzh.yap@gmail.com>
2022-01-26 14:39:23 +0900

    Fix with Lint/NonDeterministicRequireOrder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require "byebug"
 require "bundler/setup"
 require "capybara/sessionkeeper"
 
-Dir[File.join(File.dirname(__FILE__), "..", "spec", "support", "**/*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "..", "spec", "support", "**/*.rb")].sort.each {|f| require f}
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
